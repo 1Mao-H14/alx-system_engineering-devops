f240a198ee64ad12bd23871b7634d08d71887ac7
marwan <marwanhamdaoui2020@.com>
2024-08-20 10:53:23 -0700

    task 2

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,63 +1,61 @@
 #!/usr/bin/python3
-""" Module for thats containes count_occurrences function. """
+""" Module for thats contains the count_words function. """
 from requests import get
 
 
-def count_occurrences(subreddit_id, search_terms, occurrences=[], next_page=None):
-    """
-    Return the count of title inputed.
-    """
-    headers = {'User-Agent': 'HolbertonSchool'}
+def count_words(sub_name, search_terms, term_counts=[], next_page=None):
+  """
+  A function thats :
+  Returns the count of the title inputed . 
+  """
+  user_agent = {'User-Agent': 'HolbertonSchool'}
 
-    search_terms = [term.lower() for term in search_terms]
+  lower_terms = [term.lower() for term in search_terms]
 
-    if not bool(occurrences) is False:
-        for term in search_terms:
-            occurrences.append(0)
+  if not term_counts:
+    for term in lower_terms:
+      term_counts.append(0)
 
-    if next_page is None:
-        endpoint = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit_id)
-        response = get(endpoint, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            for post in response.json()['data']['children']:
-                idx = 0
-                for idx in range(len(search_terms)):
-                    for term in [t for t in post['data']['title'].split()]:
-                        term = term.lower()
-                        if search_terms[idx] == term:
-                            occurrences[idx] += 1
-                    idx += 1
+  if next_page is None:
+    api_url = 'https://www.reddit.com/r/{}/hot.json'.format(sub_name)
+    response = get(api_url, headers=user_agent, allow_redirects=False)
+    if response.status_code == 200:
+      for post in response.json()['data']['children']:
+        term_index = 0
+        for term_index in range(len(lower_terms)):
+          for word in [w for w in post['data']['title'].split()]:
+            word = word.lower()
+            if lower_terms[term_index] == word:
+              term_counts[term_index] += 1
+          term_index += 1
 
-            if response.json()['data']['after'] is not None:
-                count_occurrences(subreddit_id, search_terms,
-                                  occurrences, response.json()['data']['after'])
-    else:
-        endpoint = ('https://www.reddit.com/r/{}/hot.json?after={}'
-                    .format(subreddit_id,
-                            next_page))
-        response = get(endpoint, headers=headers, allow_redirects=False)
+      if response.json()['data']['after'] is not None:
+        count_words(sub_name, lower_terms, term_counts, response.json()['data']['after'])
+  else:
+    api_url = ('https://www.reddit.com/r/{}/hot.json?after={}'
+            .format(sub_name, next_page))
+    response = get(api_url, headers=user_agent, allow_redirects=False)
 
-        if response.status_code == 200:
-            for post in response.json()['data']['children']:
-                idx = 0
-                for idx in range(len(search_terms)):
-                    for term in [t for t in post['data']['title'].split()]:
-                        term = term.lower()
-                        if search_terms[idx] == term:
-                            occurrences[idx] += 1
-                    idx += 1
-            if response.json()['data']['after'] is not None:
-                count_occurrences(subreddit_id, search_terms,
-                                  occurrences, response.json()['data']['after'])
-            else:
-                result_counts = {}
-                for term in list(set(search_terms)):
-                    idx = search_terms.index(term)
-                    if occurrences[idx] != 0:
-                        result_counts[search_terms[idx]] = (occurrences[idx] *
-                                                             search_terms.count(search_terms[idx]))
+    if response.status_code == 200:
+      for post in response.json()['data']['children']:
+        term_index = 0
+        for term_index in range(len(lower_terms)):
+          for word in [w for w in post['data']['title'].split()]:
+            word = word.lower()
+            if lower_terms[term_index] == word:
+              term_counts[term_index] += 1
+          term_index += 1
 
-                for term, count in sorted(result_counts.items(),
-                                          key=lambda x: (-x[1], x[0])):
-                    print('{}: {}'.format(term, count))
+      if response.json()['data']['after'] is not None:
+        count_words(sub_name, lower_terms, term_counts, response.json()['data']['after'])
+      else:
+        final_counts = {}
+        for key_term in list(set(lower_terms)):
+          term_index = lower_terms.index(key_term)
+          if term_counts[term_index] != 0:
+            final_counts[lower_terms[term_index]] = (term_counts[term_index] *
+                                        lower_terms.count(lower_terms[term_index]))
 
+        for term, count in sorted(final_counts.items(),
+                                 key=lambda x: (-x[1], x[0])):
+          print('{}: {}'.format(term, count))
