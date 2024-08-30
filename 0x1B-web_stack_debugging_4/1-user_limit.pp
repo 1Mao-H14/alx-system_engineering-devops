cbb8bd913114305b8377d3550593dd1c383ce7f4
marwan <marwanhamdaoui2020@gmail.com>
2024-08-29 17:53:49 -0700

    lets go

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Enable holberton TO LOGIN.
+
+# Increase hard file MIMITE.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase file LIMITE
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
